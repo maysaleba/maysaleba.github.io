e4a633ac6522811c5f11b2a688c077eab5f44fac
maysaleba <maysalebaph@gmail.com>
2021-05-12 21:30:25 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+var phpExchange = 47.80;
+
+
+var salePriceGetter = function(params) {
+    let newSale = params.data.SalePrice * phpExchange;
+    let formatted = Math.ceil(newSale);
+    return formatted
+};
+
+var priceGetter = function(params) {
+    let newSale = params.data.Price * phpExchange;
+    let formatted = Math.ceil(newSale);
+    return formatted
+};
+
+var alltimelowGetter = function(params) {
+    let newSale = params.data.LowestPrice * phpExchange;
+    let formatted = Math.ceil(newSale);
+    return formatted
+};
+
+var ratingGetter = function(params) {
+    let newRating = params.data.SCORE
+    if (newRating == -1) {
+        return null
+    }
+    return newRating
+};
+
+
+
 var columnDefs = [{
         field: "Title",
         filter: true,
@@ -5,77 +36,161 @@ var columnDefs = [{
         lockPosition: true,
         pinned: 'left',
         cellRenderer: function(params) {
-          let keyData = params.data.Price
-          let newLink = `<a href= https://ag-grid.com/${keyData}-getting-started
-            target="_blank">${keyData}</a>`;
+            let keyData = params.value
+            let keyLink = params.data.URL
+            let newLink = `<a href= https://www.nintendo.com${keyLink}>${keyData}</a>`;
             return newLink;
-  }
-    },
-    {
-        field: "Release Date",
-        filter: true,
-        hide: true
-    },
-    {
-        field: "ESRB Rating",
-        sortable: true,
-        filter: true
-    },
-    {
-        field: "Number of Players",
-        sortable: true,
-        filter: true
-    },
-    {
-        field: "Publisher",
-        filter: true
+        }
     },
     {
         field: "Price",
         sortable: true,
         filter: true,
+        valueGetter: priceGetter,
         valueFormatter: currencyFormatter,
+        filter: 'agNumberColumnFilter',
         cellStyle: params => {
-      // you can use either came case or dashes, the grid converts to whats needed
-         return {textDecoration: 'line-through'};
-    },
+            // you can use either came case or dashes, the grid converts to whats needed
+            return {
+                textDecoration: 'line-through'
+            };
+        },
     },
     {
-        field: "Sale Price",
+        headerName: "% Off",
+        field: "PercentOff",
+        sortable: true,
+        filter: true
+    },
+    {
+        field: "SalePrice",
         sortable: true,
         filter: true,
-        valueFormatter: currencyFormatter
+
+        cellStyle: {
+            backgroundColor: '#aaffaa'
+        },
+
+        valueGetter: salePriceGetter,
+        valueFormatter: currencyFormatter,
+        filter: 'agNumberColumnFilter',
+
     },
+
     {
-        field: "Percent Off",
-        sortable: true,
-        filter: true
-    },
-    {
-        field: "Lowest Price",
-        sortable: true,
-        filter: true
-    },
-    {
-        field: "Sale Started",
+        headerName: "All Time Low",
+        field: "LowestPrice",
         sortable: true,
         filter: true,
-        sort: 'desc'
+        valueGetter: priceGetter,
+        valueFormatter: currencyFormatter,
+        filter: 'agNumberColumnFilter',
+    },
+
+    {
+        field: "SaleStarted",
+        sortable: true,
+        sort: 'desc',
+        filter: 'agDateColumnFilter',
+        valueFormatter: dateFormatter,
+        filterParams: {
+            // provide comparator function
+            comparator: (filterLocalDateAtMidnight, cellValue) => {
+                const dateAsString = cellValue;
+
+                if (dateAsString == null) {
+                    return 0;
+                }
+
+                // In the example application, dates are stored as dd/mm/yyyy
+                // We create a Date object for comparison against the filter date
+                const dateParts = dateAsString.split('-');
+                const day = Number(dateParts[2]);
+                const month = Number(dateParts[1]) - 1;
+                const year = Number(dateParts[0]);
+                const cellDate = new Date(year, month, day);
+
+                // Now that both parameters are Date objects, we can compare
+                if (cellDate < filterLocalDateAtMidnight) {
+                    return -1;
+                } else if (cellDate > filterLocalDateAtMidnight) {
+                    return 1;
+                }
+                return 0;
+            }
+        }
     },
     {
-        field: "Sale Ends",
+        field: "SaleEnds",
+        sortable: true,
+        filter: true,
+        filter: 'agDateColumnFilter',
+        valueFormatter: dateFormatter,
+        filterParams: {
+            // provide comparator function
+            comparator: (filterLocalDateAtMidnight, cellValue) => {
+                const dateAsString = cellValue;
+
+                if (dateAsString == null) {
+                    return 0;
+                }
+
+                // In the example application, dates are stored as dd/mm/yyyy
+                // We create a Date object for comparison against the filter date
+                const dateParts = dateAsString.split('-');
+                const day = Number(dateParts[2]);
+                const month = Number(dateParts[1]) - 1;
+                const year = Number(dateParts[0]);
+                const cellDate = new Date(year, month, day);
+
+                // Now that both parameters are Date objects, we can compare
+                if (cellDate < filterLocalDateAtMidnight) {
+                    return -1;
+                } else if (cellDate > filterLocalDateAtMidnight) {
+                    return 1;
+                }
+                return 0;
+            }
+        }
+    },
+    {
+        headerName: "Rating",
+        field: "SCORE",
+        sortable: true,
+        filter: true,
+        filter: 'agNumberColumnFilter',
+        valueGetter: ratingGetter,
+
+    },
+    {
+        field: "Publisher",
+        filter: true
+    },
+
+    {
+        field: "ReleaseDate",
+        filter: true,
+        hide: true
+    },
+
+    {
+        headerName: "# of Players",
+        field: "NumberofPlayers",
         sortable: true,
         filter: true
     },
+    {
+        field: "ESRBRating",
+        sortable: true,
+        filter: true
+    },
+
+
     {
         field: "URL",
         sortable: true,
-        filter: true
-    },
-    {
-        field: "SCORE",
-        sortable: true,
-        filter: true
+        filter: true,
+        hide: true
     }
 ];
 
@@ -97,6 +212,16 @@ var gridOptions = {
 
 };
 
+var saleFilterParams = {
+    allowedCharPattern: '\\d\\-\\,\\P',
+    numberParser: function(text) {
+        return text == null ?
+            null :
+            parseFloat(text.replace('P', ''));
+    },
+};
+
+
 function sizeToFit() {
     gridOptions.api.sizeColumnsToFit();
 }
@@ -111,10 +236,23 @@ function autoSizeAll(skipHeader) {
 }
 
 function currencyFormatter(params) {
-
-    return "P"+params.value
+    let peso = params.value;
+    return 'P' + formatNumber(peso);
 }
 
+function dateFormatter(params) {
+    var dateAsString = params.value;
+    var dateParts = dateAsString.split('-');
+    return `${dateParts[2]} - ${dateParts[1]} - ${dateParts[0]}`;
+}
+
+function formatNumber(number) {
+    // this puts commas into the number eg 1000 goes to 1,000,
+    // i pulled this from stack overflow, i have no idea how it works
+    return Math.floor(number)
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
 
 
 
@@ -125,7 +263,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     agGrid
         .simpleHttpRequest({
-            url: 'https://raw.githubusercontent.com/maysaleba/maysaleba.github.io/main/source.json', 
+            url: 'https://raw.githubusercontent.com/maysaleba/maysaleba.github.io/main/source.json',
         })
         .then(function(data) {
             gridOptions.api.setRowData(data);
