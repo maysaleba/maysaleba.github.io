3614456f0496008652b6784b45db90383a3eeb90
maysaleba <maysalebaph@gmail.com>
2022-04-09 09:23:28 +1200

    923

--- src/AdSense.js
+++ src/AdSense.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export default class Ad extends React.Component {
-  componentDidMount () {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-  }
-
-render () {
-    return (
-      <div className='ad'>
-        <ins className='adsbygoogle'
-          style={{ display: 'block' }}
-          data-ad-client='ca-pub-4543556906953539'
-          data-ad-slot='3566322911'
-          data-ad-format='auto'
-          data-full-width-responsive="true"
-           />
-      </div>
-    );
-  }
+const AdSense = props => {
+  const { currentPath } = props
+  
+  useEffect(() => {
+    window.adsbygoogle = window.adsbygoogle || []
+    window.adsbygoogle.push({})
+  }, [currentPath])
+  
+  return (
+    <div className='ad' key={currentPath}>
+      <ins 
+      className="adsbygoogle"
+      style={{display: 'block'}}
+      data-ad-client="ca-pub-4543556906953539"
+      data-ad-slot="3566322911"
+      data-full-width-responsive="true"
+      />
+    </div>
+  )
 }
+
+export default AdSense
