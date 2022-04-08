98f3b7e8d7cdd5af3f03b4775977fc66e2b58ca1
maysaleba <maysalebaph@gmail.com>
2022-04-09 10:33:40 +1200

    1033

--- src/Content.js
+++ src/Content.js
@@ -8,7 +8,7 @@ import styled from "styled-components";
 import download from "./download.gif";
 import {Helmet} from "react-helmet";
 import  { Redirect } from 'react-router-dom'
-import GoogleAds from "./AdSense"
+import AdSense from 'react-adsense';
 
 const YoutubeEmbed = ({ embedId }) => (
   <div className="video-responsive">
@@ -1539,7 +1539,14 @@ function HasMatch(){
             <HasHLTB props={matchGames[0]} />
             
 <Card.Body>
-<GoogleAds/>
+<AdSense.Google
+  client='ca-pub-4543556906953539'
+  slot='1687469656'
+  style={{ display: 'block' }}
+  format='auto'
+  responsive='true'
+  layoutKey='-gw-1+2a-9x+5c'
+/>
 
 </Card.Body>
           <Card.Header style={{ backgroundColor: "white", fontWeight: "bold" }}>
