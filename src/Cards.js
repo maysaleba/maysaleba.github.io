6a4597b7ecf2602e98b8dd9628f5b3958ca7699c
maysaleba <maysalebaph@gmail.com>
2022-04-10 13:16:35 +1200

    116

--- src/Cards.js
+++ src/Cards.js
@@ -18,8 +18,8 @@ import download from "./download.gif";
   today = yyyy + "-" + mm + "-" + dd;
 
 
-  // var theURL = "https://api.exchangerate.host/latest?base=PHP&v=" + today + "T" + hour;
-            var theURL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/php.json'
+  var theURL = "https://api.exchangerate.host/latest?base=PHP&v=" + today + "T" + hour;
+     // var theURL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/php.json'
             function httpGet(theUrl) {
                 var xmlHttp = new XMLHttpRequest();
                 xmlHttp.open("GET", theURL, false); // false for synchronous request
@@ -32,32 +32,32 @@ import download from "./download.gif";
             
             // console.log(fxcp.php.usd);
 
-            var usdExchange = 1 / fxcp.php.usd;
-            var arsExchange = 1 / fxcp.php.ars;
-            var audExchange = 1 / fxcp.php.aud;
-            var brlExchange = 1 / fxcp.php.brl;
-            var cadExchange = 1 / fxcp.php.cad;
-            var clpExchange = 1 / fxcp.php.clp;
-            var copExchange = 1 / fxcp.php.cop;
-            var mxnExchange = 1 / fxcp.php.mxn;
-            var penExchange = 1 / fxcp.php.pen;
-            var plnExchange = 1 / fxcp.php.pln;
-            var rubExchange = 1 / fxcp.php.rub;
-            var zarExchange = 1 / fxcp.php.zar;
+            // var usdExchange = 1 / fxcp.php.usd;
+            // var arsExchange = 1 / fxcp.php.ars;
+            // var audExchange = 1 / fxcp.php.aud;
+            // var brlExchange = 1 / fxcp.php.brl;
+            // var cadExchange = 1 / fxcp.php.cad;
+            // var clpExchange = 1 / fxcp.php.clp;
+            // var copExchange = 1 / fxcp.php.cop;
+            // var mxnExchange = 1 / fxcp.php.mxn;
+            // var penExchange = 1 / fxcp.php.pen;
+            // var plnExchange = 1 / fxcp.php.pln;
+            // var rubExchange = 1 / fxcp.php.rub;
+            // var zarExchange = 1 / fxcp.php.zar;
 
 
-            // var usdExchange = 1 / fxcp.rates.USD;
-            // var arsExchange = 1 / fxcp.rates.ARS;
-            // var audExchange = 1 / fxcp.rates.AUD;
-            // var brlExchange = 1 / fxcp.rates.BRL;
-            // var cadExchange = 1 / fxcp.rates.CAD;
-            // var clpExchange = 1 / fxcp.rates.CLP;
-            // var copExchange = 1 / fxcp.rates.COP;
-            // var mxnExchange = 1 / fxcp.rates.MXN;
-            // var penExchange = 1 / fxcp.rates.PEN;
-            // var plnExchange = 1 / fxcp.rates.PLN;
-            // var rubExchange = 1 / fxcp.rates.RUB;
-            // var zarExchange = 1 / fxcp.rates.ZAR;
+            var usdExchange = 1 / fxcp.rates.USD;
+            var arsExchange = 1 / fxcp.rates.ARS;
+            var audExchange = 1 / fxcp.rates.AUD;
+            var brlExchange = 1 / fxcp.rates.BRL;
+            var cadExchange = 1 / fxcp.rates.CAD;
+            var clpExchange = 1 / fxcp.rates.CLP;
+            var copExchange = 1 / fxcp.rates.COP;
+            var mxnExchange = 1 / fxcp.rates.MXN;
+            var penExchange = 1 / fxcp.rates.PEN;
+            var plnExchange = 1 / fxcp.rates.PLN;
+            var rubExchange = 1 / fxcp.rates.RUB;
+            var zarExchange = 1 / fxcp.rates.ZAR;
 
 
             console.log("USD\n" + usdExchange + "\nARS\n" + arsExchange + "\nAUD\n" + audExchange + "\nBRL\n" + brlExchange + "\nCAD\n" + cadExchange + "\nCLP\n" + clpExchange + "\nCOP\n" + copExchange 
