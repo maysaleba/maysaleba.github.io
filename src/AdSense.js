537c0ca26a07c2ec089c60440c7bc3b09a2f2039
maysaleba <maysalebaph@gmail.com>
2022-04-15 08:59:53 +1200

    859

--- src/AdSense.js
+++ src/AdSense.js
@@ -1,30 +1,40 @@
 import React, { useEffect } from "react"
 
-
-
-
-const AdSense = props => {
-  const { currentPath } = props
-  
+const AdSense = () => {
   useEffect(() => {
-    window.adsbygoogle = window.adsbygoogle || []
-    window.adsbygoogle.push({})
-  }, [currentPath])
-  
+    const pushAd = () => {
+      try {
+        const adsbygoogle = window.adsbygoogle
+        console.log({ adsbygoogle })
+        adsbygoogle.push({})
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    let interval = setInterval(() => {
+      // Check if Adsense script is loaded every 300ms
+      if (window.adsbygoogle) {
+        pushAd()
+        // clear the interval once the ad is pushed so that function isn't called indefinitely
+        clearInterval(interval)
+      }
+    }, 300)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
   return (
-    <div key={currentPath}>
-      <ins 
-        className="adsbygoogle example_responsive_1"
-       style={{ display: "block"}}
+    <ins
+      className="adsbygoogle example_responsive_1"
+      style={{ display: "block"}}
       data-ad-client="ca-pub-4543556906953539"
       data-ad-slot="1687469656"
-      />
-    </div>
+      enable_page_level_ads="true"
+      overlays={{bottom: true}}
+    ></ins>
   )
 }
-
-
-
-
 
 export default AdSense
