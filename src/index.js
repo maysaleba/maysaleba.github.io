e117ff3937339959fc0d3f40416e6e530aca2a1c
maysaleba <maysalebaph@gmail.com>
2022-04-15 10:05:42 +1200

    remove react-snap

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import './index.css';
 import App from './Appcopy';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { hydrate, render } from "react-dom";
- 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
 
+
+ReactDOM.render(
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
