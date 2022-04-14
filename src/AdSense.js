61d09e5afbe27cbc8c9b83fb037acdaca524151d
maysaleba <maysalebaph@gmail.com>
2022-04-15 02:54:56 +1200

    254

--- src/AdSense.js
+++ src/AdSense.js
@@ -29,8 +29,10 @@ const AdSense = () => {
     <ins
       className="adsbygoogle example_responsive_1"
       style={{ display: "block"}}
-     data-ad-client="ca-pub-4543556906953539"
-     data-ad-slot="5005582342"></ins>
+      data-ad-client="ca-pub-4543556906953539"
+      data-ad-slot="1687469656"
+      data-full-width-responsive="true"
+    ></ins>
   )
 }
 
