8dc0210758dbad970e0711166f57b229248b5bfe
maysaleba <maysalebaph@gmail.com>
2022-04-28 23:14:29 +1200

    1114

--- src/AdSense.js
+++ src/AdSense.js
@@ -1,30 +1,6 @@
 import React, { useEffect } from "react"
 
 const AdSense = () => {
-  useEffect(() => {
-    const pushAd = () => {
-      try {
-        const adsbygoogle = window.adsbygoogle
-        console.log({ adsbygoogle })
-        adsbygoogle.push({})
-      } catch (e) {
-        console.error(e)
-      }
-    }
-
-    let interval = setInterval(() => {
-      // Check if Adsense script is loaded every 300ms
-      if (window.adsbygoogle) {
-        pushAd()
-        // clear the interval once the ad is pushed so that function isn't called indefinitely
-        clearInterval(interval)
-      }
-    }, 300)
-
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
   return (
     <ins
       className="adsbygoogle example_responsive_1"
