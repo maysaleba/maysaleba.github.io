ec9b4926b7c38b412fbc5c5e369f88a53393a669
maysaleba <maysalebaph@gmail.com>
2022-04-28 23:32:38 +1200

    1132

--- src/AdSense.js
+++ src/AdSense.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react"
 
-export default class AdSense extends React.Component {
-    componentDidMount () {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
+const AdSense = () => {
+  useEffect(() => {
+    const pushAd = () => {
+      try {
+        const adsbygoogle = window.adsbygoogle
+        console.log({ adsbygoogle })
+        adsbygoogle.push({})
+      } catch (e) {
+        console.error(e)
+      }
     }
 
-    render () {
-        return (
-          
-                <ins className='adsbygoogle example_responsive_1'
-                     style={{ display: 'block' }}
-                     data-ad-client='ca-pub-4543556906953539'
-                     data-ad-slot='1687469656'
-                     >
-            </ins>
-        );
+    let interval = setInterval(() => {
+      // Check if Adsense script is loaded every 300ms
+      if (window.adsbygoogle) {
+        pushAd()
+        // clear the interval once the ad is pushed so that function isn't called indefinitely
+        clearInterval(interval)
+      }
+    }, 300)
+
+    return () => {
+      clearInterval(interval)
     }
+  }, [])
+  return (
+    <ins
+      className="adsbygoogle example_responsive_1"
+      style={{ display: "block"}}
+      data-ad-client="ca-pub-4543556906953539"
+      data-ad-slot="1687469656"
+      enable_page_level_ads="true"
+      overlays={{bottom: true}}
+    ></ins>
+  )
 }
+
+export default AdSense
