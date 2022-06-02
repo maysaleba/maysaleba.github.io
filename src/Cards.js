be84296263f5a32b89d42c2e7831503970f8a9c6
maysaleba <maysalebaph@gmail.com>
2022-06-02 20:59:08 +1200

    859

--- src/Cards.js
+++ src/Cards.js
@@ -49,7 +49,7 @@ let hour = today.getHours();
 today = yyyy + "-" + mm + "-" + dd;
 
   var theURL =
-    "https://api.exchangerate.host/latest?base=PHP&v=" + today + "T" + hour;
+    "https://a11pi.exchangerate.host/latest?base=PHP&v=" + today + "T" + hour;
   var theURLa =
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/php.json";
 
