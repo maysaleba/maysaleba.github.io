65faf5c87dcb8812cbdf06d2987db49f35ca7f17
maysaleba <maysalebaph@gmail.com>
2022-08-28 01:02:21 +1200

    102

--- src/Content.js
+++ src/Content.js
@@ -1639,7 +1639,7 @@ const Content = ({ makeswitch, datam, search, setSearch, match }) => {
                     {matchGames[0].Publisher}
                   </Col>
                   <Col style={{ paddingBottom: 10 }}>
-                    <span style={{ fontWeight: "bold" }}>Added to eShop:</span>{" "}
+                    <span style={{ fontWeight: "bold" }}>Date Added:</span>{" "}
                     {DateConvert(matchGames[0].ReleaseDate)}
                   </Col>
                 </Row>
